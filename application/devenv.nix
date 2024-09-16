261240f8895eaf10ac2798ba1c0a49f9eec3d77b
Shun Usami <usatie@gmail.com>
2024-09-16 10:23:57 -0700

    Update nginx configuration (devenv)

--- application/devenv.nix
+++ application/devenv.nix
@@ -23,12 +23,18 @@
     nginx = {
       enable = true;
       httpConfig = ''
-        server {
-          listen 8080;
-          location / {
-            return 200 "Hello, world!";
-          }
+	  server {
+        listen 8080;
+        listen [::]:8080;
+
+        location / {
+                proxy_pass http://localhost:5050;
+                proxy_set_header Host $host;
+                proxy_set_header X-Real-IP $remote_addr;
+                proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                proxy_set_header X-Forwarded-Proto $scheme;
         }
+	  }
       '';
       };
     mysql = {
