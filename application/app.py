8db37e9703fceee6c0f7b68de827b6b49ae90962
Kim Nguyen <kim.nguyen4124@gmail.com>
2024-09-19 21:19:01 -0700

    Add profile page and update about page with styling

--- application/app.py
+++ application/app.py
@@ -1,14 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, abort, url_for
 from collections import namedtuple
 
 app = Flask(__name__)
 
-Member = namedtuple("Member", ["name", "role", "profile"])
+Member = namedtuple("Member", ["name", "role", "profile", "image_url"])
 members = {
     "usatie": Member(
-        "Shun Usami", "Team Lead", "Software Engineer + Entrepreneur + Father"
+        "Shun Usami",
+        "Team Lead",
+        "Software Engineer + Entrepreneur + Father",
+        "images/usatie.jpg",
     ),
-    "some_member": Member("Some Member", "Some Role", "This is a sample profile."),
+    "AdharshT": Member(
+        "Adharsh Thiagarajan", "Frontend Lead", "Volleyball Star", "images/adharsh.jpg"
+    ),
+    "Novedh": Member(
+        "Devon Huang", "Backend Lead", "Pickleball Master", "images/devon.jpg"
+    ),
+    "kimbucha": Member(
+        "Kim Nguyen", "Github Master", "Yerba Mate Enthusiast", "images/kim.jpg"
+    ),
+    "thihaaung32": Member(
+        "Thiha Aung", "Software Developer", "Tea Salad Fanatic", "images/thiha.jpg"
+    ),
 }
 
 
@@ -24,9 +38,12 @@ def about():
 
 @app.route("/about/<name>")
 def about_member_detail(name):
-    member = members[name]
-    return render_template("member_detail.html", member=member)
+    member = members.get(name)
+    if member:
+        return render_template("member_detail.html", member=member)
+    else:
+        abort(404)
 
 
 if __name__ == "__main__":
-    app.run(port=5050)
+    app.run(port=5000, debug=True)
