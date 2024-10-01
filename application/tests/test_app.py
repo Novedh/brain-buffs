6fe7e0c864e3f8b9c3927466dc99407746f21c19
Thiha Aung <aungt3574@gmail.com>
2024-09-30 23:31:35 -0700

    add test about member detail function

--- application/tests/test_app.py
+++ application/tests/test_app.py
@@ -14,3 +14,11 @@ def test_about_page(client):
     """Test that the about page loads successfully."""
     response = client.get("/about")
     assert response.status_code == 200
+
+
+# New test case to verify that the /about/{member} page loads successfully
+def test_about_member_detail(client):
+    """Test that the member detail page for one member loads successfully."""
+    member = "thihaaung32"
+    response = client.get(f"/about/{member}")
+    assert response.status_code == 200
