59d90ee719a80b0c9def94e62bdb9c25e0eab4b9
Novedh <devonhuang11@gmail.com>
2024-11-06 22:57:45 -0800

    remove variables no longer needed

--- application/src/app.py
+++ application/src/app.py
@@ -62,7 +62,7 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template("about.html", members=members, subjects=g.subjects)
+    return render_template("about.html", members=members)
 
 
 @app.route("/search", methods=["GET"])
