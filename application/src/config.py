851d3576b92aa9a9bea44f9530bb466048754d42
Shun Usami <usatie@gmail.com>
2024-11-08 11:36:36 -0800

    Fix config.py file to remove dotenv import and load_dotenv call
    The loading of .env should be handled by flask command.

--- application/src/config.py
+++ application/src/config.py
@@ -1,18 +1,11 @@
 import mysql.connector
-from dotenv import load_dotenv
 import os
-
-load_dotenv(override=True)
 
 
 def get_db_connection():
-
     return mysql.connector.connect(
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER_NAME"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_DATABASE"),
     )
-
-
-#
