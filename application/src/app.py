7722e4ace279a8d7ed0d7ea4ff4bec14f22d168b
tuan nguyen <60669364+kimbucha@users.noreply.github.com>
2024-11-11 16:29:48 -0800

    Update app.py

--- application/src/app.py
+++ application/src/app.py
@@ -98,10 +98,6 @@ def about_member_detail(name):
         abort(404)
 
 
-@app.route("/tutor_dashboard")
-def tutor_dashboard():
-    return render_template("tutor_dashboard.html")
-
 
 if __name__ == "__main__":
     app.run(port=5050)
