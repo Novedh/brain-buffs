71d32c5d5191e808e3284843c4bc76b73c7f098a
Thiha Aung <aungt3574@gmail.com>
2024-11-18 15:09:42 -0800

    change the Author and filename

--- application/src/models/booking_requests.py
+++ application/src/models/booking_requests.py
@@ -1,5 +1,5 @@
 # Class: CSC-648-848 Fall 2024
-# Filename: xxx.py
-# Author(s): Jane Doe
+# Filename: booking_requests.py
+# Author(s): Kim Nguyen
 # Created: 2024-11-14
 # Description: This script has a baic header format.
--- application/src/models/subjects.py
+++ application/src/models/subjects.py
@@ -1,5 +1,5 @@
 # Class: CSC-648-848 Fall 2024
-# Filename: xxx.py
-# Author(s): Jane Doe
+# Filename: subjects.py
+# Author(s): Adharsh Thiagarajan
 # Created: 2024-11-14
 # Description: This script has a baic header format.
