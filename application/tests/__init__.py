66b517d812b546d08e113fa7a42da998cdfdcc49
Thiha Aung <aungt3574@gmail.com>
2024-11-19 09:30:26 -0800

    change author name on the header file

--- application/tests/__init__.py
+++ application/tests/__init__.py
@@ -1,5 +1,5 @@
 # Class: CSC-648-848 Fall 2024
-# Filename: xxx.py
-# Author(s): Jane Doe
+# Filename: __init__.py
+# Author(s): Shun Usami
 # Created: 2024-11-14
 # Description: This script has a baic header format.
