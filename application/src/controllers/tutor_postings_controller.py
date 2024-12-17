00b8ca484d11fb18f26d1113f1e3a496007986d0
Novedh <devonhuang11@gmail.com>
2024-12-16 21:31:59 -0800

    refactor tutor_posting_controller to use with

--- application/src/controllers/tutor_postings_controller.py
+++ application/src/controllers/tutor_postings_controller.py
@@ -49,62 +49,53 @@ def tutor_signup():
     profile_picture = request.files.get("profile_picture")
     cv_file = request.files.get("cv")
 
-    conn = get_db_connection()
-    cursor = conn.cursor()
-
     try:
-        # Insert tutor posting into the database, put a placeholder for urls because we dont know the post id until after
-        posting_id = create_tutor_posting(
-            cursor,
-            course_number=class_number,
-            pay_rate=pay_rate,
-            description=description,
-            profile_picture_url=None,
-            cv_url=None,
-            subject_id=subject_id,
-            user_id=int(user_id),
-            title=title,
-        )
-        # save and update the database with the correct file paths using the model method if not null
-        if profile_picture:
-            if not allowed_file(profile_picture.filename):
-                raise ValueError("Invalid profile_pic format.")
-            profile_pic_path = save_profile_picture(
-                profile_picture, posting_id, user_id
+        with get_db_connection() as conn, conn.cursor() as cursor:
+            # Insert tutor posting into the database, put a placeholder for urls because we dont know the post id until after
+            posting_id = create_tutor_posting(
+                cursor,
+                course_number=class_number,
+                pay_rate=pay_rate,
+                description=description,
+                profile_picture_url=None,
+                cv_url=None,
+                subject_id=subject_id,
+                user_id=int(user_id),
+                title=title,
             )
-            update_tutor_posting_pic_path(cursor, posting_id, profile_pic_path)
+            # save and update the database with the correct file paths using the model method if not null
+            if profile_picture:
+                if not allowed_file(profile_picture.filename):
+                    raise ValueError("Invalid profile_pic format.")
+                profile_pic_path = save_profile_picture(
+                    profile_picture, posting_id, user_id
+                )
+                update_tutor_posting_pic_path(cursor, posting_id, profile_pic_path)
 
-        if cv_file:
-            if not allowed_file(cv_file.filename):
-                raise ValueError("Invalid CV format.")
-            cv_path = save_cv(cv_file, posting_id, user_id)
-            update_tutor_posting_CV_path(cursor, posting_id, cv_path)
+            if cv_file:
+                if not allowed_file(cv_file.filename):
+                    raise ValueError("Invalid CV format.")
+                cv_path = save_cv(cv_file, posting_id, user_id)
+                update_tutor_posting_CV_path(cursor, posting_id, cv_path)
 
-        conn.commit()
-        current_app.logger.info(
-            f"Tutor posting created successfully with ID: {posting_id}"
-        )
-        flash(
-            "Tutor posting created successfully! Please wait up to 24 hours to be approved by Admins.",
-            "success",
-        )
-
+            conn.commit()
+            current_app.logger.info(
+                f"Tutor posting created successfully with ID: {posting_id}"
+            )
+            flash(
+                "Tutor posting created successfully! Please wait up to 24 hours to be approved by Admins.",
+                "success",
+            )
     except ValueError as ve:
         conn.rollback()
         current_app.logger.error(f"Failed to create tutor posting: {ve}")
-
         flash(f"Failed to create tutor posting: {ve}", "danger")
         return redirect(url_for("frontend.dashboard")), 400
-
     except Exception as e:
         conn.rollback()
         current_app.logger.error(f"Failed to create tutor posting: {e}")
-
         flash(f"Failed to create tutor posting: {e}", "danger")
         return redirect("/tutor_signup")
-    finally:
-        cursor.close()
-        conn.close()
 
     return redirect("/dashboard")
 
@@ -118,26 +109,19 @@ def delete_tutor_post(tutor_posting_id):
     if not is_logged_in():
         return redirect(url_for("user_backend.login_form", message="login_required"))
 
-    conn = get_db_connection()
-    cursor = conn.cursor()
-
     try:
-        # Attempt to delete the tutor post
-        deleted = delete_tutor_posting(cursor, tutor_posting_id, user_id)
-        if deleted:
-            conn.commit()
-            flash("Successfully deleted tutor posting!", "success")
-        else:
-            flash("Failed to delete tutor posting.", "danger")
-
+        with get_db_connection() as conn, conn.cursor() as cursor:
+            # Attempt to delete the tutor post
+            deleted = delete_tutor_posting(cursor, tutor_posting_id, user_id)
+            if deleted:
+                conn.commit()
+                flash("Successfully deleted tutor posting!", "success")
+            else:
+                flash("Failed to delete tutor posting.", "danger")
     except Exception as e:
         conn.rollback()
         current_app.logger.error(f"Error deleting tutor post: {e}")
         flash(f"Failed to delete tutor posting: {e}", "danger")
-
-    finally:
-        cursor.close()
-        conn.close()
 
     return redirect("/dashboard")
 
